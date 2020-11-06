33887d80f5df3c7532f3e68fac675766a91af4ae
Victor Brincas Sinkunas <vicropht@protonmail.com>
2020-11-06 23:22:32 +0100

    Add home page, configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { WeatherComponent } from './tools/weather/weather.component';
 
 const routes: Routes = [
-  { path: '', component: WeatherComponent },
+  { path: '', component: HomeComponent },
+  { path: 'weather', component: WeatherComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './tools/weather/weather.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
